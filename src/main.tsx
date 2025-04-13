05925244dcd47188aad663dc1555bc1a362bcf3e
LabyDev <201366893+LabyDev@users.noreply.github.com>
2025-04-13 12:17:38 +0200

    work on nav

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { NavBar } from "./NavBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <MantineProvider>
+        <NavBar />
         <Routes>
           <Route path="/" element={<App />} />
         </Routes>
