8260c23fd7e1f46214f56e6b6bc1de409ffcd4b9
LabyDev <201366893+LabyDev@users.noreply.github.com>
2025-04-13 11:30:11 +0200

    Initial commit (moved accounts)

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MantineProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </MantineProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
