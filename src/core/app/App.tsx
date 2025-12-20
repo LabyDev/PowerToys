ad6cb33dc69627fc4fe7a621e5f737283c052448
LabyDev <201366893+LabyDev@users.noreply.github.com>
2025-12-20 18:18:58 +0100

    Add page animations

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -1,24 +1,59 @@
 import { NavBar } from "./NavBar";
 import FileRandomiser from "../../file-randomiser/fileRandomiser";
 import MainPage from "../mainpage/MainPage";
-import { Routes, Route, useLocation } from "react-router";
 import Settings from "../settings/settings";
+import { Routes, Route, useLocation } from "react-router";
+import { motion, AnimatePresence } from "framer-motion";
 
 export function App() {
   const location = useLocation();
-
   const isAtRoot = location.pathname === "/";
+
+  // Page wrapper for subtle fade+slide
+  const PageWrapper = ({ children }: { children: React.ReactNode }) => (
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+      transition={{ duration: 0.1 }}
+      style={{ height: "100%" }}
+    >
+      {children}
+    </motion.div>
+  );
 
   return (
     <>
-      {/* Conditionally render the NavBar */}
-      {!isAtRoot && <NavBar />}
+      <AnimatePresence mode="wait" initial={false}>
+        {!isAtRoot && <NavBar />}
 
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/FileRandomiser" element={<FileRandomiser />} />
-        <Route path="/Settings" element={<Settings />} />
-      </Routes>
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <PageWrapper>
+                <MainPage />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/FileRandomiser"
+            element={
+              <PageWrapper>
+                <FileRandomiser />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/Settings"
+            element={
+              <PageWrapper>
+                <Settings />
+              </PageWrapper>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
